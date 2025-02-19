9a50b47e92629dbc7280064cb94801bbd7436865
fabuu4real@gmail.com <fabuu4real@gmail.com>
2025-02-19 12:12:19 +0000

    updated main.tf file

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,12 @@ output "public_ips" {
 
 */
 output "jenkins-worker-pub-ip" {
-    value = aws_instance.jenkins-worker.public_ip
+    value = aws_instance.jenkins-test-ec2.public_ip
+  
+}
+
+output "jenkins-worker-pub-ip" {
+    value = aws_instance.jenkins-test-ec2.public_dns
   
 }
 
